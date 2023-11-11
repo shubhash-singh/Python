4ec53d82e36a45b7925258f182808ade8d2fdb3e
Shubhash Singh <subhashsingh2059@gmail.com>
2023-11-11 12:30:00 +0530

    Fixed all issue in permutation iterTool

--- iterTool_Permutation.py
+++ iterTool_Permutation.py
@@ -1,4 +1,4 @@
-from itertools import combinations_with_replacement
+from itertools import permutations
 
 input_string = input()
 S = input_string.split(" ")
@@ -6,7 +6,14 @@ n = int(S[1])
 STR = S[0]
 
 result=[]
+arranged_result = []
+result = permutations(STR,n)
+#print(result)
 
-result = combinations_with_replacement(STR,2)
 for a in result:
-    print(''.join(a))
+    arranged_result.append(''.join(a))
+    
+arranged_result.sort()
+for b in arranged_result:
+    print(b)
+    

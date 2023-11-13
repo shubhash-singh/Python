1412b3593071cbf00a708bbac79c482e5d096a51
Shubhash Singh <subhashsingh2059@gmail.com>
2023-11-13 20:31:34 +0530

    Performing Operation on set

---
+++ set Operation.py
@@ -0,0 +1,14 @@
+n = int(input())
+s = set(map(int, input().split()))
+
+N = int(input())
+for i in range(N):
+    command = list(input().split(" "))
+    if command[0] == "pop":
+        s.pop()
+    elif command[0] == "remove":
+        s.remove(int(command[1]))
+    elif command[0] == "discard":
+        s.discard(int(command[1]))
+# result = [a for a in s]
+print(sum(s))

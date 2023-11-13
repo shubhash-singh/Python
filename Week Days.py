ec33c120b8e8edfd29c7c97b1c05df2de83b9a51
Shubhash Singh <subhashsingh2059@gmail.com>
2023-11-13 15:57:25 +0530

    Modification

--- Week Days.py
+++ Week Days.py
@@ -6,5 +6,5 @@ day = int(date[1])
 year = int(date[2])
 
 result = calendar.weekday(year, month, day)
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 print(weekdays[result].upper())

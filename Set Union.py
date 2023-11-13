938fd0f2713a3ae0cd1427dfdd2990c08fca85c3
Shubhash Singh <subhashsingh2059@gmail.com>
2023-11-13 20:38:03 +0530

    Set union and intersection

--- Set Union.py
+++ Set Union.py
@@ -5,3 +5,11 @@ set2 = set(map(int, input().split(" ")))
 
 result = set1 | set2
 print(len(result))
+
+n = int(input())
+set1 = set(map(int, input().split(" ")))
+m = int(input())
+set2 = set(map(int, input().split(" ")))
+
+result = set1 & set2
+print(len(result))

9e42a0e4d82ba99327fbd78c899b04ee9cf6b2c0
Shubhash Singh <subhashsingh2059@gmail.com>
2023-11-21 21:34:48 +0530

    transpose of a array

--- numpy Array transpose and flatten.py
+++ numpy Array transpose and flatten.py
@@ -1,9 +1,10 @@
 import numpy
-my_array = numpy.array([])
+my_array = []  # creating a empty list
 n, m = map(int, input().split())
 for i in range(n):
 
-    my_array += numpy.array(list(map(int, input().split())))
-
-print(numpy.transpose(my_array))
-print(my_array.flatten())
+    my_array.append(list(map(int, input().split())))
+    # adding newly got input to aa list as a list element
+my_array = numpy.array(my_array)  # creating multidimensional array
+print(numpy.transpose(my_array))  # getting transpose of the array 
+print(my_array.flatten())   # creating a single dimensional array from 2_D arary

b7e6fbe686f6e746b061bf98283bfc8ccdfe5907
Shubhash Singh <subhashsingh2059@gmail.com>
2023-11-24 20:33:21 +0530

    Any or all function of python solution

--- Adding element to set.py
+++ Adding element to set.py
@@ -1,5 +1,18 @@
-s = set()
-N = int(input())
-for i in range(N):
-    s.add(input())
-print(len(s))
+# def is_palindromic(list1):
+#     list1 = str(list1)
+#     if list1 == list1[::-1]:
+#         return True
+#     else:
+#         return False
+
+# a = int(input())
+# list1 = list(map(int, input().split()))
+# print(list1)
+# if all(a>0 for a in list1):
+#     if any(is_palindromic(elem) for elem in list1):
+#         print(True)
+# else:
+#     print(False)
+
+N,n = int(input()),input().split()
+print(all([int(i)>0 for i in n]) and any([j == j[::-1] for j in n]))

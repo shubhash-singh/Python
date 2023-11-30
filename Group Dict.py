6f348f55256fc36e6c3cc47d0f915222b02014d8
Shubhash Singh <subhashsingh2059@gmail.com>
2023-12-01 00:09:43 +0530

    Group Dict Solution

--- Group Dict.py
+++ Group Dict.py
@@ -1,10 +1,10 @@
 import re
 
 input_string = input()
-pattern = r"[a-zA-Z0-9]"
+pattern = r"([a-zA-Z0-9])\1"
 # regex to check for more than one occurrence of a digit
 m = re.search(pattern, input_string)
 if m is not None:
-    print(m.group(0))
+    print(m.group(1))
 else:
     print("-1")

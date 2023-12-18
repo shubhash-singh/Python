fb564780d74aa8dea96dd96ef6d4ba3f81c07068
Shubhash Singh <subhashsingh2059@gmail.com>
2023-12-18 16:29:57 +0530

    Decorationg Phone Number Solution

---
+++ Decorationg PhoneNumber.py
@@ -0,0 +1,13 @@
+def wrapper(f):
+    def fun(l):
+        f(["+91 " + cn[-10:-5]+" "+cn[-5:] for cn in l])
+    return fun
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
+
+
